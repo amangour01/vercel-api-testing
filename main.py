3227c4178050c53ba5e2ad0c10786f76653cc9c0
amangour01 <aman.gour@gramener.com>
2025-01-28 17:06:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello from FastAPI!"}
 
+@app.get("/api")
+def read_all():
+    return {"message": "Read all data from api"}
+
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
